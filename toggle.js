7bbb1b02edf8c41f9e89b379bbc600d8a21c5561
bilal-bin-yar <bilalbinyar@neurog.ai>
2023-06-22 14:05:06 +0500

    first commit

---
+++ toggle.js
@@ -0,0 +1,12 @@
+const wrapper = document.getElementById('theme');
+
+theme.addEventListener('change', function(e) {
+  if(e.target.checked) {
+    document.body.classList.toggle("litemode");
+  }
+  else{
+    document.body.classList.remove("litemode");
+  }
+})
+
+
